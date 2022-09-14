3fce3c9932b4f3f2316c4dc11ac9cb0139776999
Felipe Beser <felipebeserx@gmail.com>
2022-09-14 19:37:56 -0300

    comps

---
+++ src/app/app.routers.ts
@@ -0,0 +1,8 @@
+import { Routes } from "@angular/router";
+import { ContactUsComponent } from "./contact-us/contact-us.component";
+import { HeaderComponent } from "./header/header.component";
+
+
+export const ROUTERS : Routes = [
+
+];
