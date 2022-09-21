6ee14ceccc3e407c3f10cd56f1574b0fcfbeb3f5
Felipe Beser <felipebeserx@gmail.com>
2022-09-21 18:21:12 -0300

    rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HeaderComponent } from './header/header.component';
+import { ServicesPageComponent } from './services-page/services-page.component';
 
 const routes: Routes = [
-  {
+{
     path: 'header' , component: HeaderComponent
 },
 {
     path: 'contact' , component: ContactUsComponent
+},
+{
+  path: 'services' , component: ServicesPageComponent
+},
+{
+  path: 'about' , component: AboutComponent
 }
 ];
 
